49549d4f1e4ac32a1831a18029010782989015b7
holyshared <holy.shared.design@gmail.com>
2011-12-26 22:46:38 +0900

    Bootstrap -> Moostrap

--- bootstrap-package.bat
+++ bootstrap-package.bat
@@ -1,9 +1,9 @@
 cd C:\var\www\tools\packager
 
-php packager build Bootstrap/Bootstrap.Executer.Sync +use-only Bootstrap > C:\var\www\git\Bootstrap\Build\bootstrap-sync.js
-php packager build Bootstrap/Bootstrap.Executer.Async +use-only Bootstrap > C:\var\www\git\Bootstrap\Build\bootstrap-async.js
+php packager build Moostrap/Moostrap.Executer.Sync +use-only Moostrap > C:\var\www\git\Bootstrap\Build\moostrap-sync.js
+php packager build Moostrap/Moostrap.Executer.Async +use-only Moostrap > C:\var\www\git\Bootstrap\Build\moostrap-async.js
 
 cd C:\var\www\tools\yuicompressor\build
 
-java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Bootstrap\Build\bootstrap-sync-compressed.js C:\var\www\git\Bootstrap\Build\bootstrap-sync.js
-java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Bootstrap\Build\bootstrap-async-compressed.js C:\var\www\git\Bootstrap\Build\bootstrap-async.js
+java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Moostrap\Build\moostrap-sync-compressed.js C:\var\www\git\Moostrap\Build\moostrap-sync.js
+java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Moostrap\Build\moostrap-async-compressed.js C:\var\www\git\Moostrap\Build\moostrap-async.js
