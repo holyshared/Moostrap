8be672b9d507bc94b6dea7a4dbd0addcdb268e40
Noritaka Horio <holy.shared.design@gmail.com>
2012-01-02 19:10:09 +0900

    add build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+cd C:\var\www\tools\packager
+php packager build Moostrap/Moostrap.Executer.Sync +use-only Moostrap > C:\var\www\git\Moostrap\Build\moostrap-sync.js
+php packager build Moostrap/Moostrap.Executer.Async +use-only Moostrap > C:\var\www\git\Moostrap\Build\moostrap-async.js
+
+cd C:\var\www\tools\yuicompressor\build
+java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Moostrap\Build\moostrap-sync-compressed.js C:\var\www\git\Moostrap\Build\moostrap-sync.js
+java -jar yuicompressor-2.4.6.jar -o C:\var\www\git\Moostrap\Build\moostrap-async-compressed.js C:\var\www\git\Moostrap\Build\moostrap-async.js
